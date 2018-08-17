aa71e8c05bff1586fd16fffe6786a6545c251a5e
BarrosLucas <lucasfreitasdebarros@gmail.com>
2018-08-17 08:22:01 -0300

    Inserção da classe controle

---
+++ Projeto3lp1/Controle.cpp
@@ -0,0 +1,18 @@
+/*
+ * Controle.cpp
+ *
+ *  Created on: 17 de ago de 2018
+ *      Author: ci
+ */
+
+#include "Controle.h"
+
+Controle::Controle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Controle::~Controle() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Projeto3lp1/Controle.h
@@ -0,0 +1,17 @@
+/*
+ * Controle.h
+ *
+ *  Created on: 17 de ago de 2018
+ *      Author: ci
+ */
+
+#ifndef CONTROLE_H_
+#define CONTROLE_H_
+
+class Controle {
+public:
+	Controle();
+	virtual ~Controle();
+};
+
+#endif /* CONTROLE_H_ */
